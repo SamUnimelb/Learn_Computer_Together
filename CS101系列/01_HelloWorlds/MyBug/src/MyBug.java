f6ebe76b9c3fbcad7e7f22740735ca162b8edd23
Sam Yan <896298116@qq.com>
2021-10-07 22:13:34 +1100

    变更HelloWorld项目
    增添“除以0”运行错误（又称算术异常）示例。

---
+++ CS101系列/01_HelloWorlds/MyBug/src/MyBug.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Sam_Yan
+ */
+public class MyBug {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(5 / 0);
+    }
+    
+}
