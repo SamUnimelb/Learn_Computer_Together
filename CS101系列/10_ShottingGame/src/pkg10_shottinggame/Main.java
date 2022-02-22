830c21ea2a9266751fcc29baeb38c5a67ddc7b1a
Sam Yan <896298116@qq.com>
2022-02-22 22:13:40 +1100

    完成了射击游戏的原版
    完成了射击游戏的原版

--- CS101系列/10_ShottingGame/src/pkg10_shottinggame/Main.java
+++ CS101系列/10_ShottingGame/src/pkg10_shottinggame/Main.java
@@ -5,11 +5,14 @@
  */
 package pkg10_shottinggame;
 
+import java.util.Scanner;
+
 /**
  *
  * @author TR
  */
 public class Main {
+    private static Scanner input;
 
     /**
      * @param args the command line arguments
@@ -19,6 +22,57 @@ public class Main {
                new String[]{"AK47", "knife", "grenade"});
        Player samYan = new Player("S Yan", 
                new String[]{"AK47", "knife", "nuclic bomb"});
+       
+       Player[] players = {profWang, samYan};
+       
+       profWang.startGame(1200);
+       samYan.startGame(1000);
+       
+       runGame(players);
+           
+    }
+    
+    public static void runGame(Player[] players){
+        int count = 0;
+        while(!Player.onlyPlayer()){
+            int playerSeq = count % players.length;
+            System.out.println("Player: " + players[playerSeq].userName 
+                    + "'s turn: ");
+            input = new Scanner(System.in);
+            System.out.println("Input the person you want to attack: ");
+            String person = input.nextLine();
+            System.out.println("Input the equipment you want to use: ");
+            String equip = input.nextLine();
+            Player p = whoGotAttacked(players, person);
+            if(p != null){
+                AttackEvent evt = players[playerSeq].attack(p, equip);
+                p.blood -= evt.bloodDeducted();
+                System.out.println(evt);
+                
+                if(p.isCool()){
+                    System.out.println(p + "凉了！");
+                    --Player.numOfPlayers;
+                }
+            }
+            ++ count;
+        }
+        
+        congratulateMessage(players);
+    }
+    
+    public static Player whoGotAttacked(Player[] players, String person){
+        for(Player p : players){
+            //System.out.println(p.userName.equals(person));
+            if(p.userName.equals(person))
+               return p; 
+        }
+        return null;
+    }
+
+    public static void congratulateMessage(Player[] players){
+        for(Player p : players)
+            if(!p.isCool())
+                System.out.println("恭喜" + p.userName + ", 你赢了！");
     }
     
 }
--- CS101系列/10_ShottingGame/src/pkg10_shottinggame/Player.java
+++ CS101系列/10_ShottingGame/src/pkg10_shottinggame/Player.java
@@ -16,62 +16,23 @@ public class Player {
         this.blood = blood;
     }
     
-    public boolean attack(String from, String to, String equip){
+    public AttackEvent attack(Player to, String equip){
         boolean attackedSuccessfully = (int)(Math.random() * 10) >= 3;
-        int hValue = deductBlood(equip);
-        
-        if(isCool()){
-            System.out.println(to + " GAME OVER!");
-            --numOfPlayers;
-            
-            if(onlyPlayer())
-                System.out.println("Congratulations, " + from + "!");
-        }else{
-            if(attackedSuccessfully)
-                System.out.println(from + " attacked " + to 
-                        + " using " + equip + ", harm value: " + hValue);
-            else
-                System.out.println(from + " missed!"); 
-        }
-        
-        return attackedSuccessfully;
+        //In python: return (self, to, equip, attackedSuccessfully)
+        return new AttackEvent(this, to, equip, attackedSuccessfully);
     }
     
-    public boolean onlyPlayer(){
+    public static boolean onlyPlayer(){
         return numOfPlayers == 1;
     }
     
     public boolean isCool(){
-        return this.blood > 0;
+        return !(this.blood > 0);
     }
     
-    public int deductBlood(String equip) {
-        int harmValue = 0;
-        switch (equip){
-            case "AK47":
-                harmValue = 100;
-                this.blood -= 100;
-                break;
-                
-            case "grenade":
-                harmValue = 200;
-                this.blood -= 200;
-                break;
-            
-            case "knife":
-                harmValue = 30;
-                this.blood -= 30;
-                break;
-                
-            default:
-                harmValue = 1000;
-                this.blood -= 1000;
-                break;
-        }
-        
-        return harmValue;
-
+    @Override
+    public String toString(){
+        return userName;
     }
-    
-    
+ 
 }
