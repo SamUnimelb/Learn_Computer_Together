0f9cd63e7aba9d33b97cc894e35e9d36d1a9856e
Sam Yan <896298116@qq.com>
2022-05-11 20:31:16 +1000

    增加了除法的例子
    增加了LC-3下除法的例子（并在R2中给出余数）

--- CS202计算机系统基础/04_冯诺依曼结构操作/BR指令示例/Division.asm
+++ CS202计算机系统基础/04_冯诺依曼结构操作/BR指令示例/Division.asm
@@ -11,7 +11,10 @@ NOT R3, R2 ;R1 - R2
 ADD R3, R3, 1
 ADD R1, R1, R3
 BRp -5
-BRz 1 //Jump to HALT and R4 is the result
+BRz 2 //Jump to HALT and R4 is the result
 ADD R4, R4, -1
+ADD R1, R1, R2 //Reminder value in R1, but since R1 has other usages have to store in other places, e.g. R2
+AND R2, R2, 0
+ADD R2, R2, R1
 HALT
 .END
