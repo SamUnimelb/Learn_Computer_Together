c033a5ff42d61dbacffdb4048b89f2c94375621e
Sam Yan <896298116@qq.com>
2022-05-30 12:48:28 +1000

    处理了样章的部分内容
    处理了样章的部分内容

---
+++ CS202计算机系统基础/02_上溢下溢实验/NumOverflow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    unsigned int bigInt1 = pow(2, 32) - 1;
+    printf("The unsigned big integer is: %u\n", bigInt1);
+    bigInt1 ++;
+    printf("The unsigned big integer is: %u\n", bigInt1);
+
+    int bigInt2 = pow(2, 31) - 1;
+    printf("The big integer is: %d\n", bigInt2);
+    bigInt2++;
+    printf("The big integer is: %d\n", bigInt2);
+}
