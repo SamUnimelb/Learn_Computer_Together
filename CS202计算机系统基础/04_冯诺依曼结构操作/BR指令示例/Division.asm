d3367eca3ac5ddf2801dc30facc12dc810f8c63a
Sam Yan <896298116@qq.com>
2022-09-24 18:41:53 +1000

    更新汇编与C乘法除法示例程序
    目前能处理合理范围内任意整数的乘法和除法。

--- CS202计算机系统基础/04_冯诺依曼结构操作/BR指令示例/Division.asm
+++ CS202计算机系统基础/04_冯诺依曼结构操作/BR指令示例/Division.asm
@@ -1,20 +1,82 @@
-//Program for division num1 / num2
-//e.g. num1 = 10, num2 = 5, num1 / num2 = 
+//This program is able to perform division
+//of any 2 given integers whose absolute value
+//ranges from x0 ~ xFFFF.
+
+//The program gets result in R3, reminder in R2.
+//The signal of the reminder depends on the nominator.
+//e.g. num1 = 11, num2 = 5, num1 / num2 = 2...1
+//e.g. num1 = -11, num2 = 5, num1 / num2 = -2...-1
+//e.g. num1 = 11, num2 = -5, num1 / num2 = -2...1
+//e.g. num1 = -11, num2 = -5, num1 / num2 = 2...-1
+
 .ORIG x3000
-LD R1, xF
-LD R2, xF
-AND R3, R3, 0 ;Store divsor
-ADD R3, R2, 0
-AND R4, R4, 0 ;Store result.
-ADD R4, R4, 1
-NOT R3, R2 ;R1 - R2
-ADD R3, R3, 1
-ADD R1, R1, R3
-BRp -5
-BRz 2 //Jump to HALT and R4 is the result
-ADD R4, R4, -1
-ADD R1, R1, R2 //Reminder value in R1, but since R1 has other usages have to store in other places, e.g. R2
-AND R2, R2, 0
-ADD R2, R2, R1
-HALT
+AND R3 R3 0 ;Store result in R3
+
+; If both are +, R4 = 0
+; If ONLY NUM2 is -, R4 = 1
+; If ONLY NUM1 is -, R4 = 2
+; If both are -, R4 = 3
+
+AND R4 R4 0
+LD R2 NUM2 ; Store divisor and reminder in R2
+BRz MGZERODIV
+BRp GETDEN ; Jump to get denominator R1
+ADD R4 R4 1; R2 negative number originally
+NOT R2 R2  ; Make it positive number
+ADD R2 R2 1	
+
+GETDEN	LD R1 NUM1
+	BRz   SETR20
+	BRnp  KEEPGETR1
+SETR20	AND R2 R2 0
+	BRz EOP
+
+KEEPGETR1    BRp   NEGATE
+	     ADD R4 R4 2	;R1 negative number originally
+	     NOT R1 R1	;Make it positive number
+	     ADD R1 R1 1
+	     BRp   NEGATE
+        
+MGZERODIV	LD R0 ZERODIVERR	;If divisor is 0,
+		LEA R1 ZERODIVERR	;Manage to output the exception.
+	OUTPUT  PUTC
+		ADD R1 R1 1
+		LDR R0 R1 0
+BRp	OUTPUT
+BRz	EOP
+
+NEGATE NOT R2 R2
+       ADD R2 R2 1
+    
+CALCULATE   ADD R3 R3 1 
+	    ADD R1, R1, R2
+BRz CLEANR2 //No reminder and division finished!
+BRp CALCULATE
+BRn GETREM
+
+CLEANR2	AND R2 R2 0
+BRz MGSIG
+
+GETREM  ADD R3 R3 -1
+	NOT R2 R2
+	ADD R2 R2 1
+	ADD R2 R1 R2
+BRp	MGSIG
+
+MGSIG	ADD R4 R4 -1
+	BRnz GETRET
+	NOT R2 R2
+	ADD R2 R2 1	
+GETRET	ADD R4 R4 1
+	BRz EOP
+	ADD R4 R4 -3
+	BRz EOP
+	NOT R3 R3
+	ADD R3 R3 1
+	
+EOP	HALT	; End of program.
+
+NUM1 .BLKW 1
+NUM2 .BLKW 1
+ZERODIVERR .STRINGZ "Divide by 0 exception!"
 .END
--- CS202计算机系统基础/04_冯诺依曼结构操作/BR指令示例/Multiplication.asm
+++ CS202计算机系统基础/04_冯诺依曼结构操作/BR指令示例/Multiplication.asm
@@ -1,11 +1,42 @@
-//Store positive num1 at x3010 and positive num2 at x3011
-//Calculate num1 * num2 and store it to R2
+//Calculate num1 * num2 and store the result to R3
+//num1 and num2 can be any integers ranging between xFFFD ~ xFD
+//e.g. num1 = 4, num2 = 5, num1 * num2 = 20;
+//e.g. num1 = -4, num2 5, num1 * num2 = -20;
+//e.g. num1 = 4, num2 = -5, num1 * num2 = -20;
+//e.g. num1 = -4, num2 -5, num1 * num2 = 20;
+
 .ORIG x3000
-LD R0 xF //RO = 2
-LD R1 xF //R1 = 16
-AND R2, R2, 0
-ADD R2, R2, R0 //R2 = 20
-ADD R1, R1, -1 //R1 = 0
-BRp -3
-TRAP x25
+AND R3 R3 0 ;R3 to store results of the calculation.
+AND R4 R4 0 ;R4 to calculate number of neg factors.
+LD R1 NUM1
+BRz 	CLEANUP
+BRp	GETR2
+NOT R1 R1
+ADD R1 R1 1
+ADD R4 R4 1
+
+GETR2	LD R2 NUM2
+	BRz	CLEANUP
+	BRp	CALCULATE
+NOT R2 R2
+ADD R2 R2 1
+ADD R4 R4 1
+
+CALCULATE	ADD R3 R3 R2
+		ADD R1, R1, -1
+BRp CALCULATE
+
+ADD R4 R4 -1
+BRnp	CLEANUP
+NOT R3 R3
+ADD R3 R3 1
+BRn	CLEANUP
+
+CLEANUP AND R2 R2 0
+	AND R1 R1 0
+	AND R4 R4 0
+EOP HALT
+
+NUM1 .BLKW 1
+NUM2 .BLKW 1
 .END
