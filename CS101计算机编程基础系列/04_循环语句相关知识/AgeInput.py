e20aa980a6bb1a09ffcca7e5b6ad78ca4b0e014c
SamUnimelb <896298116@qq.com>
2023-07-28 22:13:39 +1000

    增添输入年龄的while示例程序
    增添输入年龄的while示例程序

---
+++ CS101计算机编程基础系列/04_循环语句相关知识/AgeInput.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 28 22:10:14 2023
+
+@author: Sam_Yan
+"""
+
+isSatisfiedInput = False
+
+while not isSatisfiedInput:
+    try:
+        age = int(input("请亲告诉我一下年龄："))
+        isSatisfiedInput = True
+    except ValueError:
+        print("请亲确定输入十个代表年龄的整数哟！")
+    
+print("输入的年龄为：{:3d}".format(age))
---
+++ CS101计算机编程基础系列/04_循环语句相关知识/EasyInput.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 28 22:09:29 2023
+@author: Sam_Yan
+"""
+
+age = int(input("请亲告诉我一下年龄："))
