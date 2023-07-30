ef9069d5d0039c69ac39dc9450979700b5656698
SamUnimelb <896298116@qq.com>
2023-07-30 22:55:46 +1000

    增添接受正确价格输入的C++程序
    增添接受正确价格输入的C++程序

---
+++ CS101计算机编程基础系列/04_循环语句相关知识/CppTakeUserInput/TakePriceInput.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+bool isFloat(string myString){
+    std::istringstream iss(myString);
+    float f;
+    iss >> noskipws >> f; // noskipws considers leading whitespace invalid
+    // Check the entire string was consumed and if either failbit or badbit is set
+    //cout << "In function: " << (iss.eof() && !iss.fail()) << endl;
+    //system("pause");
+    return iss.eof() && !iss.fail(); 
+}
+
+bool isPriceNum(string priceStr){
+    int len = priceStr.length();
+    int dotPoint = 0;
+
+    for(int i = 0; i < len; i++){
+        if(priceStr.at(i) == '.')
+            dotPoint = i;
+    }
+
+    return len - 1 - dotPoint <= 2;
+    
+}
+
+
+int main(){
+    //Test codes:
+
+    /*printf("%d\n", isFloat("3.142"));
+    printf("%d\n", isFloat("3.14.2"));
+    printf("%d\n", isFloat("3.jbk")); 
+    printf("%d\n", isFloat("3"));*/
+
+    /*printf("%d\n", isPriceNum(3.142));
+    printf("%d\n", isPriceNum(3.14));
+    printf("%d\n", isPriceNum(3.1));
+    printf("%d\n", isPriceNum(3.148));*/
+    
+    string priceStr;
+    double price = 0.0;
+    bool isSatisfiedInput = false;
+
+    do{
+        cout << "请输入物品的价格（元）: ";
+        cin >> priceStr;
+        try{
+            //cout << "In main: " << isFloat(priceStr) << endl;
+            
+            if(isFloat(priceStr)){
+
+                price = stof(priceStr);
+
+                if(!isPriceNum(priceStr))
+                   throw(price);
+                
+                isSatisfiedInput = true;
+
+            }else{
+                //cout << "In else: " << endl;
+                //system("pause");
+                
+                /*
+                //C++中的catch子句区分处理对象是string和const char *
+                //所以实现中要么catch子句处理类型为const char *，
+                //要么抛出时将类型转化为string。
+                throw(string("请亲确定输入的是个数哦！"))*/;
+
+                throw("请亲确定输入的是个数哦！");
+            }
+        }/*catch(string typeErr){
+            cout << typeErr << endl;
+            system("pause");*/
+        catch(const char * typeErr){
+            cout << typeErr << endl;
+        }catch(double wrongPrice){
+            cout << "物品价格格式错误哦！" << endl;
+        }
+    }while(!isSatisfiedInput);
+
+    printf("正确输入的价格为：%.2f\n", price);
+
+    system("pause");
+    return 0;
+}
