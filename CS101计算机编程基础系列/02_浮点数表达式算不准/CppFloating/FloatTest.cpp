64ceb3e2a256bc383901fa7673c8d56b2327f1f1
SamUnimelb <896298116@qq.com>
2023-07-30 22:00:41 +1000

    增添了浮点数表达式问题示例程序
    增添了浮点数表达式问题示例程序

---
+++ CS101计算机编程基础系列/02_浮点数表达式算不准/CppFloating/FloatTest.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    printf("%f\n", 0.1 + 0.1 + 0.1);
+    printf("%d\n", 0.1 + 0.1 + 0.1 == 0.3);
+    printf("%f\n", 0.25 + 0.5);
+    printf("%d\n", 0.25 + 0.5 == 0.75);
+    system("pause");
+    return 0;
+}
---
+++ CS101计算机编程基础系列/02_浮点数表达式算不准/FloatCalculation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 30 16:27:24 2023
+
+@author: Sam_Yan
+"""
+
+print(0.1 + 0.1 + 0.1)
+print(0.1 + 0.1 + 0.1 == 0.3)
+print(0.25 + 0.5)
+print(0.25 + 0.5 == 0.75)
