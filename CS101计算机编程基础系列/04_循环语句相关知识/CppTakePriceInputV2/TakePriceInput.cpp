d030c6208ca6efbedfe1ea27c44880e59198f8ac
SamUnimelb <896298116@qq.com>
2023-07-31 10:36:27 +1000

    移除获取价格程序中的异常处理
    现由于用正则检查了输入的类型，故不需要进行类型检查了。

--- CS101计算机编程基础系列/04_循环语句相关知识/CppTakePriceInputV2/TakePriceInput.cpp
+++ CS101计算机编程基础系列/04_循环语句相关知识/CppTakePriceInputV2/TakePriceInput.cpp
@@ -13,20 +13,16 @@ int main(){
     do{
         cout << "请输入物品的价格（元）: ";
         cin >> priceStr;
-        try{
-            //正则表达式测试网站：https://regex101.com/
-            //R"()"表示生字符串
-            regex priceMatch(R"(^\d*(.\d{1,2})?$)");
 
-            if(regex_match(priceStr, priceMatch)){
-                price = stof(priceStr);
-                isSatisfiedInput = true;
-            }else{
-                throw("请亲确定输入的是个正确的表示价格的数哦！");
-            }
-        } catch(const char * typeErr){
-            cout << typeErr << endl;
+        regex priceMatch(R"(^\d*(.\d{1,2})?$)");
+
+        if(regex_match(priceStr, priceMatch)){
+            price = stof(priceStr);
+            isSatisfiedInput = true;
+        }else{
+            printf("%s\n", "请亲确定输入的是个正确的表示价格的数哦！");
         }
+
     }while(!isSatisfiedInput);
 
     printf("正确输入的价格为：%.2f\n", price);
